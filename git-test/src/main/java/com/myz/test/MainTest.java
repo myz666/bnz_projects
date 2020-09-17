17f7c331b8d64c42fa772028c292e8c8c1743a82
myz <2417688748@qq.com>
2020-09-17 14:41:18 +0800

    修改主函数为hello,world2

--- git-test/src/main/java/com/myz/test/MainTest.java
+++ git-test/src/main/java/com/myz/test/MainTest.java
@@ -2,7 +2,7 @@ package com.myz.test;
 
 public class MainTest {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World2");
     }
 
 
